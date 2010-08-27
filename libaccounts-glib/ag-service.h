4eb26d4a78977565c232fe467b9ec817f38f808d
smita (none) <smita@smita-laptop.>
2010-08-27 15:04:35 +0300

    Fetchin translation catalog from the service xml files

--- libaccounts-glib/ag-service.h
+++ libaccounts-glib/ag-service.h
@@ -36,6 +36,7 @@ const gchar *ag_service_get_display_name (AgService *service);
 const gchar *ag_service_get_service_type (AgService *service);
 const gchar *ag_service_get_provider (AgService *service);
 const gchar *ag_service_get_icon_name (AgService *service);
+const gchar *ag_service_get_i18n_domain (AgService *service);
 void ag_service_get_file_contents (AgService *service,
                                    const gchar **contents,
                                    gsize *data_offset);
