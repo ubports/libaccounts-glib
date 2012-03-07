22e8aee1d90c52d8790fc015a6712c444db90eaf
Alberto Mardegan <mardy@users.sourceforge.net>
2012-03-07 13:30:53 +0200

    Tests: use dbus-test-runner, if available

--- tests/accounts-glib-test.sh
+++ tests/accounts-glib-test.sh
@@ -8,4 +8,12 @@ export ACCOUNTS=/tmp/
 export AG_DEBUG=all
 export G_MESSAGES_DEBUG=all
 
-accounts-glib-testsuite "$@"
+# If dbus-test-runner exists, use it to run the tests in a separate D-Bus
+# session
+if command -v dbus-test-runner > /dev/null ; then
+    echo "Using dbus-test-runner"
+    dbus-test-runner -m 180 -t accounts-glib-testsuite
+else
+    echo "Using existing D-Bus session"
+    accounts-glib-testsuite "$@"
+fi
