dd3d84c6cac8e5ce4f996127cfccffcdfc5a44a5
David King <david.king@canonical.com>
2012-07-20 17:38:17 +0100

    Add an include to fix an implicit declaration
    Add an include for ag-provider.h to fix an implicit function
    declaration in ag-tool.

--- tools/main.c
+++ tools/main.c
@@ -25,6 +25,7 @@
 
 #include "libaccounts-glib/ag-account.h"
 #include "libaccounts-glib/ag-manager.h"
+#include "libaccounts-glib/ag-provider.h"
 #include "libaccounts-glib/ag-service.h"
 #include <stdio.h>
 #include <stdlib.h>
