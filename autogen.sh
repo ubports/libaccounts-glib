ee506d536004309464a2f8177341a85ed9d16a96
David King <david.king@canonical.com>
2013-01-23 08:31:34 +0000

    Add NOCONFIGURE logic to autogen.sh
    Additionally, fix the script to run gtkdocize from $srcdir.

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,12 @@ test -z "$srcdir" && srcdir=.
 
 (test -f $srcdir/libaccounts-glib.pc.in ) || {
 	echo -n "Error: Directory "\`$srcdir\`" does not look like the "
-        echo "to-level libaccounts-glib directory."
+        echo "top-level libaccounts-glib directory."
 	exit 1
 }
 
+cd "$srcdir"
 gtkdocize --copy --flavour no-tmpl
-autoreconf --install --force
-. $srcdir/configure "$@"
+cd "$OLDPWD"
+autoreconf --install --force --verbose --warnings=all "$srcdir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
